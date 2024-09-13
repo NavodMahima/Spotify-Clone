29b78424734703d500eeb6f33d139fd1d9fda9b3
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-09-13 23:53:12 +0530

    Player

--- spotify-clone/src/components/Player.jsx
+++ spotify-clone/src/components/Player.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import { assets, songsData } from '../assets/assets'
+import { assets } from '../assets/assets'
+import { useContext } from 'react'
+import { PlayerContext } from '../context/playerContext'
 
 const Player = () => {
+  const { track, seekBar, seekBg, PlayStatus, play, pause, time } = useContext(PlayerContext);
+
   return (
     <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
       <div className='hidden lg:flex items-center gap-4'>
-        <img className='w-12' src={songsData[0].image} alt="" />
+        <img className='w-12' src={track.image} alt="" />
         <div>
-            <p>{songsData[0].name}</p>
-            <p>{songsData[0].desc.slice(0,12)}</p>
+          <p>{track.name}</p>
+          <p>{track.desc?.slice(0, 12)}</p> 
         </div>
+        
         <div className='flex flex-col items-center gap-1 m-auto'>
           <div className='flex gap-4'>
             <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt="" />
             <img className='w-4 cursor-pointer' src={assets.prev_icon} alt="" />
-            <img className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+            
+            {!PlayStatus
+            ?<img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt="" />
+            :<img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt="" />
+            }
+            
             <img className='w-4 cursor-pointer' src={assets.next_icon} alt="" />
             <img className='w-4 cursor-pointer' src={assets.loop_icon} alt="" />
           </div>
+
           <div className='flex items-center gap-5'>
-            <p>1.06</p>
-            <div className='w-[60vw] max-w-[500px] bg-green-300 rounded-full cursor-pointer:'>
-              <hr className='h-1 border-none w-0 bg-green-800 rounded-full'/>
+
+            <p>{time?.currentTime?.minute || '00'}:{time?.currentTime?.second || '00'}</p>
+            <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-green-300 rounded-full cursor-pointer'>
+              <hr ref={seekBar} className='h-1 border-none w-0 bg-green-800 rounded-full' />
             </div>
-            <p>3.20</p>
+          
+            <p>{time?.totalTime?.minute || '00'}:{time?.totalTime?.second || '00'}</p> 
           </div>
         </div>
+
         <div className='hidden lg:flex items-center gap-2 opacity-75'>
           <img className='w-4' src={assets.play_icon} alt="" />
           <img className='w-4' src={assets.mic_icon} alt="" />
@@ -38,7 +51,7 @@ const Player = () => {
         </div>
       </div> 
     </div>
-  )
+  );
 }
 
-export default Player
+export default Player;
